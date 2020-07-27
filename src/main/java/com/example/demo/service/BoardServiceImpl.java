29ee7fc24a5ed8bc4ad97999b7b1192c44776835
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-07-27 23:46:40 +0900

    200727 Mon 23:45, 수업시간에 적은 코드와 주석이 다 날아가서 다시 적음..

--- src/main/java/com/example/demo/service/BoardServiceImpl.java
+++ src/main/java/com/example/demo/service/BoardServiceImpl.java
@@ -7,8 +7,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+// 클래스 implements 인터페이스
+// Service 어노테이션
+// Spring <<- Event Driven 방식으로 동작을 하고 있음
 @Service
 public class BoardServiceImpl implements BoardService {
+    // 클래스 사용할 때 ? 객체가 필요하다.
+    // new를 해야 한다는 의미
+    // Autowired가 붙으면 이것을 자동으로 해줌
     @Autowired
     private BoardRepository boardRepository;
 
@@ -20,5 +26,20 @@ public class BoardServiceImpl implements BoardService {
     @Override
     public List<Board> list() throws Exception {
         return boardRepository.list();
+    }
+
+    @Override
+    public Board read(Integer boardNo) throws Exception {
+        return boardRepository.read(boardNo);
+    }
+
+    @Override
+    public void remove(Integer boardNo) throws Exception {
+        boardRepository.remove(boardNo);
+    }
+
+    @Override
+    public void modify(Board board) throws Exception {
+        boardRepository.modify(board);
     }
 }
