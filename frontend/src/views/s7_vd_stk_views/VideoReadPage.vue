b77968941a200a7030842a17e486cc83f7c14e2b
Green-Pea-Ai <yachtboatfor@gmail.com>
2020-09-10 23:53:48 +0900

    200910 Thur 11:52, 개인 프로젝트.
    정처기 공부중..
    CRUD, C 진행중..

---
+++ frontend/src/views/s7_vd_stk_views/VideoReadPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="center">
+
+    <h2>Detailed Video List(Read)</h2>
+    <video-read v-if="video" :video="video"/>
+    <p v-else>Loading...</p>
+    <router-link :to="{ name: 'VideoModifyPage', params: { videoNo } }">
+      [Modify]
+    </router-link>
+
+    <button @click="onVideoDelete">Delete</button>
+    <router-link :to="{ name: 'VideoListPage'}">
+      [List]
+    </router-link>
+
+  </div>
+</template>
+
+<script>
+import VideoRead from '@/components/s7_vd_stk_comp/VideoRead'
+import { mapState, mapActions } from 'vuex'
+import axios from 'axios'
+
+export default {
+  name: 'VideoReadPage',
+  props: {
+    videoNo: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    ...mapState([
+      'video'
+    ])
+  }
+}
+</script>
